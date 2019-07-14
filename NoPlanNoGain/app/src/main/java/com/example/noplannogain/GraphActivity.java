5bf5355894c1d7785fcbb370048bccd19be5a90b
robin <robin.sgeorges@gmail.com>
2019-07-14 22:23:22 +0200

    graph working

--- NoPlanNoGain/app/src/main/java/com/example/noplannogain/GraphActivity.java
+++ NoPlanNoGain/app/src/main/java/com/example/noplannogain/GraphActivity.java
@@ -1,21 +1,34 @@
 package com.example.noplannogain;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.drawable.Drawable;
 import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 
 import com.example.noplannogain.Model.GraphEntry;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.Utils;
 
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class GraphActivity extends AppCompatActivity {
+public class GraphActivity extends AppCompatActivity implements OnChartGestureListener, OnChartValueSelectedListener {
     LineChart lineChart;
     GraphEntry graphEntry;
 
@@ -27,18 +40,118 @@ public class GraphActivity extends AppCompatActivity {
 
         lineChart = (LineChart)findViewById(R.id.linechart);
 
+        lineChart.setOnChartGestureListener(GraphActivity.this);
+        lineChart.setOnChartValueSelectedListener(GraphActivity.this);
+
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(false);
+        lineChart.setDrawGridBackground(true);
+
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+//
+//
+//        ArrayList<Entry> x = new ArrayList<Entry>();
+//        ArrayList<Entry> y = new ArrayList<Entry>();
+//
+//        LineDataSet lineDataSet1 = new LineDataSet(x, "TIME");
+
         DatabaseHelper dh = new DatabaseHelper(this);
         ArrayList entities =  dh.getGraphDatas();
 
         for(int i=0; i<entities.size();i++){
-            GraphEntry entry = (GraphEntry) entities.get(i);
-            Date dateEntry = entry.getDate();
-            int poidEntry = entry.getValue();
-            int k=0;
+            GraphEntry graphEntry = (GraphEntry) entities.get(i);
+            Date dateEntry = graphEntry.getDate();
+            int poidEntry = graphEntry.getValue();
+
+            Entry entry = new Entry(i , poidEntry);
+            entries.add(entry);
         }
 
-        List<Entry> entries = new ArrayList<Entry>();
+        LineDataSet set1;
+        if (lineChart.getData() != null &&
+                lineChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) lineChart.getData().getDataSetByIndex(0);
+            set1.setValues(entries);
+            lineChart.getData().notifyDataChanged();
+            lineChart.notifyDataSetChanged();
+        } else {
+            set1 = new LineDataSet(entries, "Poid");
+            set1.setDrawIcons(false);
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.DKGRAY);
+            set1.setCircleColor(Color.DKGRAY);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(true);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+            if (Utils.getSDKInt() >= 18) {
+                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_blue);
+                set1.setFillDrawable(drawable);
+            } else {
+                set1.setFillColor(Color.DKGRAY);
+            }
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+            LineData data = new LineData(dataSets);
+            lineChart.setData(data);
+        }
 
+
+
+    }
+
+    @Override
+    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
 
     }
 }
